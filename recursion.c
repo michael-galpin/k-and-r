7ea9771344cc1b5aef2b217ecfd661c5e3a35299
Michael Galpin <mike.sr@gmail.com>
2012-03-16 12:13:24 -0700

    It works!

--- recursion.c
+++ recursion.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void itoa(int n, char s[]);
-void itoa_(int n, int i, char s[]);
+int itoa(int n, char s[]);
 
-int main()
+int main(int argc, char* argv[])
 {
+    int input = 123;
+    if (argc >= 2){
+        input = atoi(argv[1]);
+    }
     char s[10] = "";
-    itoa(123, s);
+    itoa(input, s);
     printf("%s\n", s);
     return 0;
 }
 
-void itoa(int n, char s[]){
-    itoa_(n, 0, s);
-}
-
-void itoa_(int n, int i, char s[])
-{
-    printf("itoa_(%d, %d, %s)\n", n,  i, s);
+int itoa(int n, char s[]){
+    int i = 0;
+    int pn = n;
     if (n < 0){
         s[i++] = '-';
+        pn = -1 * n;
     }
     if (n/10){
-        itoa_(n/10, ++i, s);
-        s[i++] = n % 10 + '0';
-        printf("s=%s\n",s);
-    } else {
-        s[i] = '\0';
-    }
+        i = itoa(n/10, s);
+    } 
+    s[i++] = (pn % 10) + '0';
+    s[i] = '\0';
+    return i;
 }
