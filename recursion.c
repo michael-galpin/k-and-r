b913d5a6ad2e45086033ad1a119ba540d566f918
Michael Galpin <mike.sr@gmail.com>
2012-03-16 17:39:56 -0700

    Reverse a string in place using recursion

--- recursion.c
+++ recursion.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 
 int itoa(int n, char s[]);
+void reverse(char s[]);
+void reverse_(char s[], int start, int end);
 
 int main(int argc, char* argv[])
 {
@@ -12,6 +14,8 @@ int main(int argc, char* argv[])
     char s[10] = "";
     itoa(input, s);
     printf("%s\n", s);
+    reverse(s);
+    printf("reversed %s\n",s);
     return 0;
 }
 
@@ -29,4 +33,24 @@ int itoa(int n, char s[])
     s[i++] = (pn % 10) + '0';
     s[i] = '\0';
     return i;
+}
+
+void reverse(char s[])
+{
+    int i;
+    for (i=0;s[i] != '\0'; i++);
+    reverse_(s,0,i);
+}
+
+void reverse_(char s[], int start, int end)
+{
+    int len = end - start;
+    if (len > 1){
+        int temp = s[end-1];
+        s[end-1] = s[start];
+        s[start] = temp;
+        if (len > 2){
+            reverse_(s, start+1, end-1);
+        }
+    }
 }
