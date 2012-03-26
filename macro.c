4c9091a5f896e1e537bf33be499a829d6847846a
Michael Galpin <mike.sr@gmail.com>
2012-03-25 22:36:27 -0700

    New quiz!

--- macro.c
+++ macro.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-#define swap(t,x,y) {t temp = y; y = x; x = temp;}
+#define swap(t,x,y) {t z = y; y = x; x = z;}
 
 int main()
 {
 	int x = 4;
 	int y = 2;
-	swap(int,x,y);
+	swap(int,x,y)
 	printf("x=%d y=%d\n",x,y);
 }
